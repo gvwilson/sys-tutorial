4c0c14ab79599f3cd29b6cd4e3dbbc51c129557f
Greg Wilson <gvwilson@third-bit.com>
2024-02-15 08:01:45 -0500

    feat: starting website for copying

--- config.py
+++ config.py
@@ -17,12 +17,21 @@ rouge_style = "github.css"
 lang = "en"
 extension = "/"
 
+# Directories to copy verbatim.
+copydir = [
+    "out",
+    "site",
+    "src",
+]
+
 # Files to copy verbatim.
 copy = [
+    "*.html",
     "*.out",
     "*.py",
     "*.sh",
     "*.text",
+    "*.txt",
 ]
 
 # Enable various Markdown extensions.
